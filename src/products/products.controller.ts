07750b2f030e1a46bdb9970f249c52aa88f9ff8b
Sam <samcp210@gmail.com>
2024-08-06 18:02:10 -0500

    first commit

---
+++ src/products/products.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationDto } from 'src/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  // @Post()
+  @MessagePattern({ cmd: 'create-product' })
+  create(@Payload() createProductDto: CreateProductDto) {
+    return createProductDto;
+
+    // return this.productsService.create(createProductDto);
+  }
+
+  // @Get()
+  @MessagePattern({ cmd: 'get-products' })
+  findAll(@Payload() paginationDto: PaginationDto) {
+    return this.productsService.findAll(paginationDto);
+  }
+
+  // @Get(':id')
+  @MessagePattern({ cmd: 'get-product' })
+  findOne(@Payload('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(+id);
+  }
+
+  // @Patch(':id')
+  @MessagePattern({ cmd: 'update-product' })
+  update(
+    // @Payload('id') id: string,
+    @Payload() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsService.update(updateProductDto.id, updateProductDto);
+  }
+
+  // @Delete(':id')
+  @MessagePattern({ cmd: 'remove-product' })
+  remove(@Payload('id', ParseIntPipe) id: number) {
+    return this.productsService.remove(id);
+  }
+}
